d9002638e8ccb86a1a86caf9fd0aaf56add7ce29
Chris <c5keith90@gmail.com>
2014-04-15 08:34:07 -0600

    Piece is up

---
+++ Piece.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.awt.*;
+class Piece {
+	Position position;
+	Boolean team;
+
+    public Piece(Boolean t, Position p){
+        team = t;
+        position = p;
+    }
+
+	public ArrayList<Position> possibleMoves(){
+        ArrayList<Position> d = new ArrayList<Position>();
+        d.add(position);
+        return d;
+	}
+
+	public void removeFromGrid(){
+		position = new Position(-1, -1);
+	}
+
+	public void move(Position p){
+		position = p;
+	}
+    
+
+}
