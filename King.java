064dd20402ac982e4f3cf3e807f7c1340a30a012
arcticwaffle <bradarnot@gmail.com>
2014-04-17 08:56:52 -0600

    Updates

--- King.java
+++ King.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class King extends Piece {
 	
 	public King() {
@@ -6,13 +8,17 @@ class King extends Piece {
 
 	public ArrayList<Position> possibleMoves(){
         ArrayList<Position> d = new ArrayList<Position>();
+        ArrayList<Position> c = new ArrayList<Position>();
+        d.add(super.position());
+        c.add(super.position());
         for (int i = -1; i <= 1; i++) {
         	for (int j = -1; j <= 1; j++) {
-        		if (position().x() + i <= 8 && position().y() + j <= 8 && super.board.checkCheck(d, super.team()) && position.x() + i >= 0 && position.y() + j >= 0)
+                        c.add(new Position(super.position().x() + i, super.position().y() + j));
+        		if (position().x() + i <= 8 && position().y() + j <= 8 && super.board.checkCheck(c, super.team()) && position.x() + i >= 0 && position.y() + j >= 0)
         			d.add(new Position(super.position().x() + i, super.position().y() + j));
+                        c.remove(1);
         	}
         }
-        d.add(super.position());
         return d;
 	}
 
--- Queen.java
+++ Queen.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Queen extends Piece {
 	
 	public Queen() {
@@ -6,21 +8,25 @@ class Queen extends Piece {
 
 	public ArrayList<Position> possibleMoves(){
         ArrayList<Position> d = new ArrayList<Position>();
+        ArrayList<Position> c = new ArrayList<Position>();
+        d.add(super.position());
+        c.add(super.position());
         for (int i = -1; i <= 1; i++) {
         	for (int j = -1; j <= 1; j++) {
         		int scalar = 1;
-                        // Position position = new Position(super.position().x(), super.position().y());
-        		while (super.position().x() + (i * scalar) <= 8 && super.position().y() + (j * scalar) <= 8 && super.position().x() + (i * scalar) >= 0 && super.position().y() + (j * scalar) >= 0 && super.getPiece(new Position(super.position().x() + (i * scalar), super.position().y() + (j * scalar))) == null) {
-        		      if (super.board.checkCheck(d, super.team()))
-        		              d.add(new Position(super.position().x() + (i * scalar), super.position().y() + (j * scalar)));
+                        Position position = new Position(super.position().x() + (i * scalar), super.position().y() + (j * scalar));
+                        c.add(position);
+        		while (super.position().x() + (i * scalar) <= 8 && super.position().y() + (j * scalar) <= 8 && super.position().x() + (i * scalar) >= 0 && super.position().y() + (j * scalar) >= 0 && super.getPiece(position) == null) {
+        		      if (super.board.checkCheck(c, super.team()))
+        		              d.add(position);
                               scalar++;
                         }
-                        if (super.getPiece(new Position(super.position().x() + (i * scalar), super.position().y() + (j * scalar))) != null) {
-                           d.add(new Position(super.position().x() + (i * scalar), super.position().y() + (j * scalar)));     
+                        if (super.getPiece(position) != null) {
+                           d.add(position);     
                         }
+                        c.remove(1);
         	}
         }
-        d.add(super.position());
         return d;
 	}
 
