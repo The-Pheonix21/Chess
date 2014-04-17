9c69455772b586bbe6eaf779caee84a281274db0
Perkins <Perkinstd@s.dcsdk12.org>
2014-04-17 09:21:29 -0600

    Updated

--- Bishop.java
+++ Bishop.java
@@ -10,7 +10,7 @@ class Bishop extends Piece {
 	        ArrayList<Position> d = new ArrayList<Position>();
 	        for (int i = ; i; i++) {
 	        	Position temp = new Position(super.position.(), super.position.());
-	        	if () {
+	        	if (temp.x() >=  && temp.x()<= && temp.y() >=   && temp.y()<=) {
 	        		
 	        	}
 	        return d;
--- Rook.java
+++ Rook.java
@@ -8,7 +8,7 @@ class Rook extends Piece {
 	        ArrayList<Position> d = new ArrayList<Position>();
 	        for (int i = ; i; i++) {
 	        	Position temp = new Position(super.position.x(), super.position.y());
-	        	if () {
+	        	if (temp.x() >=  && temp.x()<= && temp.y() >=   && temp.y()<=) {
 	        		
 	        	}
 	        return d;
