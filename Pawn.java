f1204c3eeda89f17ddde46eb0435dc41fadbde41
NickPfeufer <nicholaspfeufer@msn.com>
2014-04-17 09:05:06 -0600

    remove checkCheck

--- Pawn.java
+++ Pawn.java
@@ -1,13 +1,13 @@
 class Pawn extends Piece{
 	public ArrayList<Position> possibleMoves(){
         ArrayList<Position> d = new ArrayList<Position>();
-        ArrayList<Position> check = new ArrayList<Position>();
+       
         for (int i = 0; i<3; i++) {
-        	check.clear();
-        	check.add(super.position);
+        	
+        	
         	Position temp = new Position(super.position.x() - 1 + i, super.position.y() + 1));
-			check.add(temp);
-            if(temp.x() >= 0 && temp.x()<=7 && temp.y() >= 0  && temp.y()<=7 && super.board.checkCheck(check, super.team)){
+			
+            if(temp.x() >= 0 && temp.x()<=7 && temp.y() >= 0  && temp.y()<=7){
             	if (temp.x() == position.x() && super.board.getPiece(temp) == null) {
             		d.add(temp);
             	}
