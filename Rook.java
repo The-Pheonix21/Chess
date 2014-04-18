8ce4e95a28e75a9ed9cf766d3d6af113563bf2b5
Perkins <Perkinstd@s.dcsdk12.org>
2014-04-18 08:38:17 -0600

    Complete

--- Rook.java
+++ Rook.java
@@ -6,15 +6,56 @@ class Rook extends Piece {
 
 	}
 
-		public ArrayList<Position> possibleMoves(){
-	        ArrayList<Position> d = new ArrayList<Position>();
-	        for (int i = ; i; i++) {
-	        	Position temp = new Position(super.position.x(), super.position.y());
-	        	if (temp.x() >=  && temp.x()<= && temp.y() >=   && temp.y()<=) {
-	        		
+	public ArrayList<Position> possibleMoves(){
+        ArrayList<Position> d = new ArrayList<Position>();
+        for (int i = 0; i>8; i++) {
+        	Position temp = new Position(super.position.x()+1, super.position.y()+1);
+        	if (super.board.getPiece(temp)!=null) {
+	        	if (super.board.getPiece(temp).team()!=super.team()) {
+	        		d.add(temp);
+	        		i=9;
+	        	}else{
+	        		i=9;
 	        	}
-	        }
-	        return d;
-	    }
-
+        	}
+        	d.add(temp);
+        }
+        for (int i = 0; i>8; i++) {
+        	Position temp = new Position(super.position.x()+1, super.position.y()-1);
+        	if (super.board.getPiece(temp)!=null) {
+	        	if (super.board.getPiece(temp).team()!=super.team()) {
+	        		d.add(temp);
+	        		i=9;
+	        	}else{
+	        		i=9;
+	        	}
+        	}
+        	d.add(temp);
+        }
+        for (int i = 0; i>8; i++) {
+        	Position temp = new Position(super.position.x()-1, super.position.y()+1);
+        	if (super.board.getPiece(temp)!=null) {
+	        	if (super.board.getPiece(temp).team()!=super.team()) {
+	        		d.add(temp);
+	        		i=9;
+	        	}else{
+	        		i=9;
+	        	}
+        	}
+        	d.add(temp);
+        }
+        for (int i = 0; i>8; i++) {
+        	Position temp = new Position(super.position.x()-1, super.position.y()-1);
+        	if (super.board.getPiece(temp)!=null) {
+	        	if (super.board.getPiece(temp).team()!=super.team()) {
+	        		d.add(temp);
+	        		i=9;
+	        	}else{
+	        		i=9;
+	        	}
+        	}
+        	d.add(temp);
+        }
+        return d;
+    }
 }
