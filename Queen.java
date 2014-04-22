7bc720a00ec4c6488c2f54801f9887567a25867f
arcticwaffle <bradarnot@gmail.com>
2014-04-22 09:01:23 -0600

    NICK IS STUPID

--- Queen.java
+++ Queen.java
@@ -8,14 +8,11 @@ class Queen extends Piece {
 
 	public ArrayList<Position> possibleMoves(){
         ArrayList<Position> d = new ArrayList<Position>();
-        ArrayList<Position> c = new ArrayList<Position>();
         d.add(super.position());
-        c.add(super.position());
         for (int i = -1; i <= 1; i++) {
         	for (int j = -1; j <= 1; j++) {
         		int scalar = 1;
-                        Position position = new Position(super.position().x() + (i * scalar), super.position().y() + (j * scalar));
-                        c.add(position);
+                Position position = new Position(super.position().x() + (i * scalar), super.position().y() + (j * scalar));
         		while (super.position().x() + (i * scalar) <= 8 && super.position().y() + (j * scalar) <= 8 && super.position().x() + (i * scalar) >= 0 && super.position().y() + (j * scalar) >= 0 && super.getPiece(position) == null) {
         		      d.add(position);
                               scalar++;
@@ -23,7 +20,6 @@ class Queen extends Piece {
                         if (super.getPiece(position) != null && super.getPiece(position).team() == !super.team()) {
                            d.add(position);     
                         }
-                        c.remove(1);
         	}
         }
         return d;
