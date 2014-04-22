fbe14251d2cbf7b3ffdc067cd80ef844494a7fc6
arcticwaffle <bradarnot@gmail.com>
2014-04-22 08:57:04 -0600

    Nick Sucks

--- King.java
+++ King.java
@@ -11,11 +11,9 @@ class King extends Piece {
         d.add(super.position());
         for (int i = -1; i <= 1; i++) {
         	for (int j = -1; j <= 1; j++) {
-                        Postion p = new Position(super.position().x() + i, super.position().y() + j);
-                        c.add(position);
+                        Positon p = new Position(super.position().x() + i, super.position().y() + j);
         		if (position().x() + i <= 8 && position().y() + j <= 8  && position.x() + i >= 0 && position.y() + j >= 0 && (super.board.getPiece(position) == null || super.board.getPiece(position).team() == !super.team()))
         			d.add(position);
-                        c.remove(1);
         	}
         }
         return d;
