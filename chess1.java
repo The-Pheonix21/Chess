94eef7c7fa2971ed8e80b9c2604063c36e8d38f9
NickPfeufer <nicholaspfeufer@msn.com>
2014-04-22 08:06:08 -0600

    piece implimintation

--- chess1.java
+++ chess1.java
@@ -10,8 +10,36 @@ public class chess1 extends JFrame implements MouseListener, MouseMotionListener
     int xAdjustment;
     int yAdjustment;
     Position oldpos;
+    Board board;
     public chess1(String title)
     {
+    board = new Board();
+    new King(board, 0, new Position(4, 0));
+    new King(board, 1, new Position(4, 7));
+    for (int i = 0; i<8; i++) {
+        new Pawn(board, 0, new Position(i, 1));
+    }
+    for (int i = 0; i<8; i++) {
+        new Pawn(board, 1, new Position(i, 6));
+    }
+    new Rook(board, 0, new Position(0,0));
+    new Rook(board, 0, new Position(7,0));
+    new Rook(board, 1, new Position(0,7));
+    new Rook(board, 1, new Position(7,7));
+
+    new Knight(board, 0, new Position(1,0));
+    new Knight(board, 0, new Position(6,0));
+    new Knight(board, 1, new Position(1,7));
+    new Knight(board, 1, new Position(6,7));
+
+    new Bishop(board, 0, new Position(2,0));
+    new Bishop(board, 0, new Position(5,0));
+    new Bishop(board, 1, new Position(2,7));
+    new Bishop(board, 1, new Position(5,7));
+
+    new Queen(board, 0, new Position(3, 0));
+    new Queen(board, 0, new Position(3, 7));
+
     super(title);
     Dimension boardSize = new Dimension(800, 600);
 
