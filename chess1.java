e1decdb69cdd4cf512c1b27a29dd90f944d50b65
Chris <c5keith90@gmail.com>
2014-04-22 07:40:13 -0600

    ok

---
+++ chess1.java
@@ -0,0 +1,191 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class chess1 extends JFrame implements MouseListener, MouseMotionListener
+ {
+    JLayeredPane layeredPane;
+    JPanel chessBoard;
+    JLabel chessPiece;
+    int xAdjustment;
+    int yAdjustment;
+    public chess1(String title)
+    {
+    super(title);
+    Dimension boardSize = new Dimension(800, 600);
+
+    layeredPane = new JLayeredPane();
+    getContentPane().add(layeredPane);
+    layeredPane.setPreferredSize(boardSize);
+    layeredPane.addMouseListener(this);
+    layeredPane.addMouseMotionListener(this);
+
+    chessBoard = new JPanel();
+    layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
+    chessBoard.setLayout( new GridLayout(8, 8) );
+    chessBoard.setPreferredSize( boardSize );
+    chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
+     for (int i = 0; i < 64; i++) {
+     JPanel square = new JPanel( new BorderLayout() );
+     chessBoard.add( square );
+      int row = (i / 8) % 2;
+      if (row == 0)
+     square.setBackground( i % 2 == 0 ? Color.black : Color.white );
+     else
+     square.setBackground( i % 2 == 0 ? Color.white : Color.black );
+     }
+    JLabel piece = new JLabel( new ImageIcon("3.png") );
+    JPanel panel = (JPanel)chessBoard.getComponent(0);
+    panel.add(piece);
+    piece = new JLabel(new ImageIcon("5.png"));
+    panel = (JPanel)chessBoard.getComponent(1);
+    panel.add(piece);
+    piece = new JLabel(new ImageIcon("4.png"));
+        panel = (JPanel)chessBoard.getComponent(2);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("1.jpg"));
+        panel = (JPanel)chessBoard.getComponent(3);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("2.png"));
+        panel = (JPanel)chessBoard.getComponent(4);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("4.png"));
+        panel = (JPanel)chessBoard.getComponent(5);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("5.png"));
+        panel = (JPanel)chessBoard.getComponent(6);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("3.png"));
+        panel = (JPanel)chessBoard.getComponent(7);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("6.png"));
+        panel = (JPanel)chessBoard.getComponent(8);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("6.png"));
+        panel = (JPanel)chessBoard.getComponent(9);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("6.png"));
+        panel = (JPanel)chessBoard.getComponent(10);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("6.png"));
+        panel = (JPanel)chessBoard.getComponent(11);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("6.png"));
+        panel = (JPanel)chessBoard.getComponent(12);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("6.png"));
+        panel = (JPanel)chessBoard.getComponent(13);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("6.png"));
+        panel = (JPanel)chessBoard.getComponent(14);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("6.png"));
+        panel = (JPanel)chessBoard.getComponent(15);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("12.png"));
+        panel = (JPanel)chessBoard.getComponent(48);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("12.png"));
+        panel = (JPanel)chessBoard.getComponent(49);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("12.png"));
+        panel = (JPanel)chessBoard.getComponent(50);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("12.png"));
+        panel = (JPanel)chessBoard.getComponent(51);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("12.png"));
+        panel = (JPanel)chessBoard.getComponent(52);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("12.png"));
+        panel = (JPanel)chessBoard.getComponent(53);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("12.png"));
+        panel = (JPanel)chessBoard.getComponent(54);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("12.png"));
+        panel = (JPanel)chessBoard.getComponent(55);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("9.png"));
+        panel = (JPanel)chessBoard.getComponent(56);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("11.png"));
+        panel = (JPanel)chessBoard.getComponent(57);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("10.png"));
+        panel = (JPanel)chessBoard.getComponent(58);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("7.png"));
+        panel = (JPanel)chessBoard.getComponent(59);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("8.png"));
+        panel = (JPanel)chessBoard.getComponent(60);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("10.png"));
+        panel = (JPanel)chessBoard.getComponent(61);
+        panel.add(piece);        
+        piece = new JLabel(new ImageIcon("11.png"));
+        panel = (JPanel)chessBoard.getComponent(62);
+        panel.add(piece);
+        piece = new JLabel(new ImageIcon("9.png"));
+        panel = (JPanel)chessBoard.getComponent(63);
+        panel.add(piece);
+
+    }
+
+    public void mousePressed(MouseEvent e)
+    {
+        chessPiece = null;
+        Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
+         if (c instanceof JPanel) 
+         return;
+         Point parentLocation = c.getParent().getLocation();
+        xAdjustment = parentLocation.x - e.getX();
+        yAdjustment = parentLocation.y - e.getY();
+        chessPiece = (JLabel)c;
+        chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
+        chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
+        layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
+    }
+   
+    public void mouseDragged(MouseEvent me) {
+        if (chessPiece == null) return;
+         chessPiece.setLocation(me.getX() + xAdjustment, me.getY() + yAdjustment);
+     }
+     
+    public void mouseReleased(MouseEvent e) {
+        if(chessPiece == null) return;
+
+         chessPiece.setVisible(false);
+        Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
+        if (c instanceof JLabel){
+            Container parent = c.getParent();
+            parent.remove(0);
+            parent.add( chessPiece );
+        }
+        else {
+            Container parent = (Container)c;
+            parent.add( chessPiece );
+        }
+        chessPiece.setVisible(true);
+    }
+    public void mouseClicked(MouseEvent e) {
+  
+    }
+    public void mouseMoved(MouseEvent e) {
+   }
+    public void mouseEntered(MouseEvent e){
+  
+    }
+    public void mouseExited(MouseEvent e) {
+  
+    }
+    public static void main(String[] args) {
+        JFrame frame = new chess1("Chess Board");
+        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE );
+        frame.pack();
+        frame.setResizable(true);
+        frame.setLocationRelativeTo( null );
+        frame.setVisible(true);
+     }
+}
--- chessBoard.java
+++ chessBoard.java
@@ -2,11 +2,14 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class chessGrid extends JFrame implements MouseListener{
+// public class chessBoard extends JFrame implements MouseListener, MouseMotionListener
+public class chessBoard extends JFrame implements MouseListener
+ {
     JLayeredPane layeredPane;
     JPanel chessBoard;
 
-    public chessGrid(String title){
+    public chessBoard(String title)
+    {
     super(title);
     Dimension boardSize = new Dimension(600, 600);
     Dimension boardwButton = new Dimension(600,700);
@@ -16,23 +19,12 @@ public class chessGrid extends JFrame implements MouseListener{
     layeredPane.setPreferredSize(boardwButton);
     layeredPane.addMouseListener(this);
     // layeredPane.addMouseMotionListener(this);
-    JButton teamW = new JButton("White");
-    teamW.setBackground(Color.red);
-    teamW.setBounds (700,625,75,50);
-   
-
-    JButton teamB = new JButton("Black");
-    teamB.setBackground(Color.black);
-    teamB.setBounds (600,625,75,50);
-   
-
-    // TextField winLossCounter = new JTextField("Win" +w+ "loss" +l+);
-    // win
     JButton surrender = new JButton("Surrender");
     surrender.setBackground(Color.red);
     surrender.setBounds(50,625,100,50);
     surrender.addActionListener(new ActionListener() {
-    public void actionPerformed(ActionEvent e){
+    public void actionPerformed(ActionEvent e)
+    {
         System.exit(0);
     }
     });
@@ -53,15 +45,23 @@ public class chessGrid extends JFrame implements MouseListener{
        square[i].setBackground( i % 2 == 0 ? Color.white : Color.black );
     }
   }
-     public void mousePressed(MouseEvent e){
-
+     public void mousePressed(MouseEvent e)
+     {
+       
      }
+   
+   //  public void mouseDragged(MouseEvent me) {
+       
+   //   }
+     
      public void mouseReleased(MouseEvent e) {
        
      }
      public void mouseClicked(MouseEvent e) {
   
      }
+   //  public void mouseMoved(MouseEvent e) {
+   // }
      public void mouseEntered(MouseEvent e){
   
      }
@@ -69,7 +69,7 @@ public class chessGrid extends JFrame implements MouseListener{
   
      }
     public static void main(String[] args) {
-        JFrame frame = new chessGrid("Chess Board");
+        JFrame frame = new chessBoard("Chess Board");
         frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE );
         frame.pack();
         frame.setResizable(true);
