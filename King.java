63ebcef6f4235d1af67a2307c4e80f4a970a726a
arcticwaffle <bradarnot@gmail.com>
2014-04-22 09:09:16 -0600

    STudpd

--- King.java
+++ King.java
@@ -7,7 +7,7 @@ class King extends Piece {
         d.add(super.position());
         for (int i = -1; i <= 1; i++) {
         	for (int j = -1; j <= 1; j++) {
-                        Positon p = new Position(super.position().x() + i, super.position().y() + j);
+                        Position p = new Position(super.position().x() + i, super.position().y() + j);
         		if (position().x() + i <= 8 && position().y() + j <= 8  && position.x() + i >= 0 && position.y() + j >= 0 && (super.board.getPiece(position) == null || super.board.getPiece(position).team() == !super.team()))
         			d.add(position);
         	}
