a35191800492d66e4b94e982643a7e9423707f19
NickPfeufer <nicholaspfeufer@msn.com>
2014-05-01 09:55:49 -0600

    Update King.java

--- King.java
+++ King.java
@@ -7,7 +7,6 @@ class King extends Piece {
 
 	public ArrayList<Position> possibleMoves(){
         ArrayList<Position> d = new ArrayList<Position>();
-        d.add(super.position());
         for (int i = -1; i <= 1; i++) {
         	for (int j = -1; j <= 1; j++) {
                         Position position = new Position(super.position().x() + i, super.position().y() + j);
