dd1a0fba384fcb57093fee75dedc0f9b64286d5f
NickPfeufer <nicholaspfeufer@msn.com>
2014-05-13 08:55:33 -0600

    Update Board.java

--- Board.java
+++ Board.java
@@ -53,10 +53,13 @@ class Board {
 		for (Position p : allPossibleMoves) {
 			for (Piece king : kings) {
 				if(p.equals(exclude.get(1)) && king.position().equals(exclude.get(0))){
+					exclude.clear();
 					return true;
 				}
 				if(king.team() != team && king.position().equals(p) && !p.equals(exclude.get(0))){
+					exclude.clear();
 					return true;
+					
 				}
 			}
 		}
